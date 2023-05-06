d05314ed70618f96acdd82d2f9a4a6c5cc60ff43
JovicaB <112812542+JovicaB@users.noreply.github.com>
2023-05-06 08:35:06 +0200

    Update connect.py

--- connection/connect.py
+++ connection/connect.py
@@ -1,12 +1,2 @@
 import MySQLdb
 
-def mysql_connect():
-    connection=MySQLdb.connect(
-        password="NlXCS1Tk6ilqKBc9gEvm",
-        database="railway", 
-        user="root", 
-        port=5920,
-        host="containers-us-west-172.railway.app",
-        charset='utf8mb4'
-    )
-    return connection
